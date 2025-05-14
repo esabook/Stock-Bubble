b528f22feec6f8113afa50b220372add8a735840
Egit <egit.saputra@bareksa.com>
2025-05-14 23:28:10 +0700

    Update app version v1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.stockbubble.dev"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "v1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
